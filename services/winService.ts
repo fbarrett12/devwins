37a339eda80f78ea8b73360d3d94cff18b43e834
FredrickBarrett <f.barrett12@gmail.com>
2026-06-15 21:02:58 -0500

    Added initial service layer

---
+++ services/winService.ts
@@ -0,0 +1,31 @@
+import { Category } from "@prisma/client";
+import {
+  CreateWinInput,
+  UpdateWinInput,
+  winRepository,
+} from "@/repositories/winRepository";
+
+export const winService = {
+  getAllWins() {
+    return winRepository.findAll();
+  },
+
+  getWinById(id: string) {
+    return winRepository.findById(id);
+  },
+
+  createWin(data: CreateWinInput) {
+    return winRepository.create({
+      ...data,
+      technologies: data.technologies ?? [],
+    });
+  },
+
+  updateWin(id: string, data: UpdateWinInput) {
+    return winRepository.update(id, data);
+  },
+
+  deleteWin(id: string) {
+    return winRepository.delete(id);
+  },
+};
